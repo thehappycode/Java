500ea9e4f61d118cc65c95855233b3721022b70a
thehappycode <thehappycode@hotmail.com>
2025-08-19 21:13:33 +0700

    #observer-pattern - head first - Add project weather-station

---
+++ src/core/design-patterns/head-first/observer-pattern/weather-station/WeatherStation.java
@@ -0,0 +1,10 @@
+package weatherstation;
+
+public class WeatherStation{
+  
+  public static void main(String[] args){
+    System.out.println("-> Welcome to WeatherStation program");
+    System.out.println();
+
+  }
+}
