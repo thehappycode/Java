e42665ab864e1ac4666919290bf869376f3bb00e
thehappycode <thehappycode@hotmail.com>
2025-08-19 23:16:37 +0700

    #logging - spring boot in pracetice - Add project logging tìm hiểu về logs trong spring boot

---
+++ src/spring-boot/spring-boot-in-pracetice/logging/src/test/java/com/thehappycode/logging/LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thehappycode.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
