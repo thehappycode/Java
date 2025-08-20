4b743f2f5105b9ce027c5f81bef3775106856415
thehappycode <thehappycode@hotmail.com>
2025-08-20 23:11:31 +0700

    #validation - spring boot in practice - Update bean validations

--- src/spring-boot/spring-boot-in-pracetice/validation/src/main/java/com/thehappycode/validation/ValidationApplication.java
+++ src/spring-boot/spring-boot-in-pracetice/validation/src/main/java/com/thehappycode/validation/ValidationApplication.java
@@ -1,6 +1,7 @@
 package com.thehappycode.validation;
 
 import com.thehappycode.validation.model.Course;
+import com.thehappycode.validation.model.User;
 import jakarta.validation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,15 +23,42 @@ public class ValidationApplication {
 
     @Bean
     public CommandLineRunner run() {
-        Course course = new Course(1, "", "Science", 8);
-        logger.info("-> " + course.toString());
+
+        logger.info("-> End CommandLineRunner");
+
         Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
-        Set<ConstraintViolation<Course>> violations = validator.validate(course);
-        violations.forEach(courseConstraintViolation
+
+//        TODO: Course
+        Course course = new Course(1, "", "Science", 8);
+        Set<ConstraintViolation<Course>> courseViolations = validator.validate(course);
+        courseViolations.forEach(courseConstraintViolation
                 -> logger.error(
                     "-> A constraint violation has occurred. Violation detail: [{}].",
                     courseConstraintViolation)
         );
+
+//        TODO: Users
+        User user1 = new User("tamnt1", "sbip");
+        Set<ConstraintViolation<User>> userViolations = validator.validate(user1);
+        logger.error("Password for user1 do not adhere to the password policy");
+        userViolations.forEach(constraintViolation -> logger.error("Violation details: [{}].", constraintViolation.getMessage()));
+
+        User user2 = new User("tamnt2", "Sbip01$4UDfg");
+        userViolations = validator.validate(user2);
+        if(userViolations.isEmpty()) {
+            logger.info("Password for user2 adhere to the password policy");
+        }
+
+        User user3 = new User("tamnt3", "Sbip01$4UDfgggg");
+        userViolations = validator.validate(user3);
+        logger.error("Password for user3 violates maximum repetitive rule");
+        userViolations.forEach(constraintViolation -> logger.error("Violation details: [{}].", constraintViolation.getMessage()));
+
+        User user4 = new User("tamnt4", "Sbip014UDfgggg");
+        userViolations = validator.validate(user4);
+        logger.error("Password for user4 violates special character rule");
+        userViolations.forEach(constraintViolation -> logger.error("Violation details: [{}].", constraintViolation.getMessage()));
+
         return args -> {
          logger.info("-> End CommandLineRunner");
         };
---
+++ src/spring-boot/spring-boot-in-pracetice/validation/src/main/java/com/thehappycode/validation/model/User.java
@@ -0,0 +1,31 @@
+package com.thehappycode.validation.model;
+
+import com.thehappycode.validation.rulevalidator.Password;
+
+public class User {
+    private  String userName;
+
+    @Password
+    private String password;
+
+    public User(String userName, String password){
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User {" +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
