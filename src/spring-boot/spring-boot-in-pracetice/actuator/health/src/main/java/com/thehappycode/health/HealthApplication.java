bb7c84d89afceae6bf4b54c4d6549cbd00bcdeff
thehappycode <thehappycode@hotmail.com>
2025-09-10 22:55:34 +0700

    #actuator - update project health

--- src/spring-boot/spring-boot-in-pracetice/actuator/health/src/main/java/com/thehappycode/health/HealthApplication.java
+++ src/spring-boot/spring-boot-in-pracetice/actuator/health/src/main/java/com/thehappycode/health/HealthApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ActuatorApplication {
+public class HealthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ActuatorApplication.class, args);
+		SpringApplication.run(HealthApplication.class, args);
 	}
 
 }
