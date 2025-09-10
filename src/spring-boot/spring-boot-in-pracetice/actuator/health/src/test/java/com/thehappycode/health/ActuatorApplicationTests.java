c35392c45e6b333496ed783ea67e4b85795cffbc
thehappycode <thehappycode@hotmail.com>
2025-09-10 22:40:17 +0700

    #actuator - Add project healty

---
+++ src/spring-boot/spring-boot-in-pracetice/actuator/health/src/test/java/com/thehappycode/health/ActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thehappycode.health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
