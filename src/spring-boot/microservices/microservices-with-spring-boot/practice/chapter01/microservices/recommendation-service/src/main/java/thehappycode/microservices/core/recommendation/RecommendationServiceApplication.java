224e641a383bc3e18656ea604e4ec6fd3e8db331
Nguyễn Thanh Tâm <thehappycode@hotmail.com>
2025-11-28 14:03:49 +0700

    #microservices: Add thêm project api, util

--- src/spring-boot/microservices/microservices-with-spring-boot/practice/chapter01/microservices/recommendation-service/src/main/java/thehappycode/microservices/core/recommendation/RecommendationServiceApplication.java
+++ src/spring-boot/microservices/microservices-with-spring-boot/practice/chapter01/microservices/recommendation-service/src/main/java/thehappycode/microservices/core/recommendation/RecommendationServiceApplication.java
@@ -2,8 +2,10 @@ package thehappycode.microservices.core.recommendation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+// @ComponentScan("thehappycode")
 public class RecommendationServiceApplication {
 
 	public static void main(String[] args) {
